b3da689946c7d820533b0eb7ae3eddfaf9f3bfdc
kinnaamf <dimoje.1337@gmail.com>
2025-10-25 18:57:44 +0300

    added success or error status

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,9 +5,23 @@
   </Head>
   <div class="relative min-h-screen">
 
+    <!--    <Transition name="slide-fade">-->
+    <!--      <div class="absolute right-0 top-12 bg-red-600 p-4 rounded-l-md max-w-2/12" v-if="error">-->
+    <!--        <h3 class="text-md text-white font-medium">{{ error }}</h3>-->
+    <!--      </div>-->
+    <!--    </Transition>-->
+
+    <!--    <Transition name="slide-fade">-->
+    <!--      <div class="absolute right-0 top-12 bg-emerald-600 p-4 rounded-l-md max-w-2/12" v-if="message">-->
+    <!--        <h3 class="text-md text-white font-medium">{{ message }}</h3>-->
+    <!--      </div>-->
+    <!--    </Transition>-->
+
     <Transition name="slide-fade">
-      <div class="absolute right-0 top-12 bg-red-600 p-4 rounded-l-md max-w-2/12" v-if="error">
-        <h3 class="text-md text-white font-semibold">{{ error }}</h3>
+      <div class="absolute right-0 top-12 p-4 rounded-l-md max-w-2/12 text-white"
+           :class="messageClass"
+           v-if="message.title">
+        {{ message.title }}
       </div>
     </Transition>
 
@@ -20,7 +34,11 @@
 
       <Transition name="task">
         <div class="flex justify-between items-center bg-white px-4 py-4 rounded-md shadow-md" v-if="showTask">
-          <input type="text" @keyup.enter="storeTask" placeholder="Enter new task" v-model="newTask.title"
+          <input id="task--title"
+                 type="text"
+                 @keyup.enter="storeTask"
+                 placeholder="Enter new task"
+                 v-model="newTask.title"
                  @input="error = null"
                  class="border border-gray-400 w-10/12 rounded-md py-2 h-10 pl-4">
           <button @click="storeTask" class="w-max bg-slate-700 text-white rounded-md h-10 px-6 font-normal uppercase">
@@ -34,12 +52,10 @@
           <div v-for="task in tasks" :key="task.id"
                class="flex items-center justify-between bg-white px-4 py-2 rounded-md shadow-md">
             <div class="flex gap-4 items-center w-full mr-2">
+
               <input type="checkbox" v-model="task.done" @change="toggleDone(task)">
-              <h3 class="text-2xl font-medium"
-                  :class="task.done ? 'text-gray-400 line-through transition-all duration-100' : 'text-gray-900 transition-all duration-100'"
-                  v-if="!task.editable"
-                  @dblclick="editTask(task)"
-              >
+
+              <h3 class="text-2xl font-medium" :class="taskDone(task)" v-if="!task.editable" @dblclick="editTask(task)">
                 {{ task.title }}
               </h3>
               <input type="text"
@@ -53,21 +69,26 @@
                      @keyup.enter="updateTask(task)"
               >
             </div>
-            <div class="flex gap-2 items-center" v-if="!task.editable">
-              <button @click="editTask(task)" class="bg-green-600 text-white p-2 rounded-md">
+
+            <div class="flex gap-2 items-center">
+
+              <button @click="editTask(task)" class="bg-green-600 text-white p-2 rounded-md" v-if="!task.editable">
                 <PencilIcon/>
               </button>
-              <button @click="deleteTask(task.id)" class="bg-red-500 text-white p-2 rounded-md">
+              <button @click="deleteTask(task.id)" class="bg-red-500 text-white p-2 rounded-md" v-if="!task.editable">
                 <TrashIcon/>
               </button>
-            </div>
-            <div class="flex gap-2 items-center" v-if="task.editable">
-              <button @click="updateTask(task)" class="bg-green-600 text-white p-2 rounded-md">
+
+
+              <button @click="updateTask(task)" class="bg-green-600 text-white p-2 rounded-md" v-if="task.editable">
                 <CheckIcon/>
               </button>
-              <button @click="task.editable = !task.editable" class="bg-red-500 text-white p-2 rounded-md">
+              <button @click="task.editable = !task.editable"
+                      class="bg-red-500 text-white p-2 rounded-md"
+                      v-if="task.editable">
                 <CloseIcon/>
               </button>
+
             </div>
           </div>
         </TransitionGroup>
@@ -114,22 +135,39 @@ const showTask = ref<boolean>(false);
 
 const tasks = ref<Array<Task>>([]);
 
-const error = ref<string | null>(null);
+const message = reactive({
+  title: null,
+  status: "success" | "error",
+})
+
+const taskDone = (task: Task) => {
+  return task.done ? 'text-gray-400 line-through transition-all duration-100' : 'text-gray-900 transition-all duration-100'
+}
+
+const messageClass = computed(() => {
+  return message.status == "success" ? 'bg-emerald-600' : 'bg-red-600';
+})
 
 const getTasks = async () => {
   try {
     tasks.value = await $fetch("http://localhost:5000/tasks");
   } catch (e) {
-    error.value = "Could not fetch data"
+    Object.assign(message, {
+      title: "Could not fetch data",
+      status: "error",
+    })
   }
 }
 
 const storeTask = async () => {
   const title = newTask.title.trim()
   if (!title) {
-    error.value = "Enter task name"
+    Object.assign(message, {
+      title: "Enter task name",
+      status: "error",
+    })
     return;
-  } else error.value = null;
+  }
 
   try {
     const res = await $fetch(`http://localhost:5000/tasks`, {
@@ -140,10 +178,17 @@ const storeTask = async () => {
       }
     })
     tasks.value.push(res);
-
+    nextTick(() => document.querySelector('#task--title').focus())
+    Object.assign(message, {
+      title: "Task added successfully",
+      status: "success",
+    })
   } catch (e: any) {
     if (e.message.includes("NetworkError")) {
-      error.value = "Server is unavailable at the moment. Try again later.";
+      Object.assign(message, {
+        title: "Server is unavailable at the moment. Try again later",
+        status: "error",
+      })
     }
   }
 
@@ -163,8 +208,15 @@ const updateTask = async (task: Task) => {
       }
     })
     task.editable = false;
+    Object.assign(message, {
+      title: "Task edited successfully",
+      status: "success",
+    })
   } catch (e) {
-    error.value = "Could not update data"
+    Object.assign(message, {
+      title: "Could not update data",
+      status: "error",
+    })
   }
 }
 
@@ -183,8 +235,15 @@ const deleteTask = async (id: string) => {
       method: "DELETE",
     })
     tasks.value = tasks.value.filter(task => task.id !== id)
+    Object.assign(message, {
+      title: "Task deleted successfully",
+      status: "success",
+    })
   } catch (e) {
-    error.value = "Could not delete task"
+    Object.assign(message, {
+      title: "Could not delete task",
+      status: "error",
+    })
   }
 }
 </script>
@@ -260,4 +319,5 @@ input[type="checkbox"]:checked {
 .task-leave-to {
   @apply opacity-100 translate-x-0;
 }
+
 </style>
